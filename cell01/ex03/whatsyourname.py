6bc66d886cc7a12db92f85c4ed876c63eead8f85
karlun-chinda <gungunguy17902@gmail.com>
2026-02-10 20:42:59 +0700

    Update file permissions and improve input handling in various scripts

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-name = input("Hey, what's your first name? : ")
-surname = input("And your last name? : ")
+name = input("Hey, what's your first name? : ").strip()
+surname = input("And your last name? : ").strip()
 print("Well, pleased to meet you, " + name + " " + surname + ".")
